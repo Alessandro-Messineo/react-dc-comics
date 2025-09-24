66e31f8888ee4ea2823842fb22c8b47a4a3bb0c5
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-24 17:29:39 +0200

    creato header

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import logo from '../assets/img/dc-logo.png';
+
+const Header = () => {
+    return (
+        <header>
+            <div class='container-header'>
+                <img src={logo} alt="dc-logo" class='logo' />
+                <nav class='navbar-header'>
+                    <ul >
+                        <li>
+                            <a href="">CHARACTERS</a>
+                        </li>
+                        <li>
+                            <a href="">COMICS</a>
+                        </li>
+                        <li>
+                            <a href="">MOVIES</a>
+                        </li>
+                        <li>
+                            <a href="">TV</a>
+                        </li>
+                        <li>
+                            <a href="">GAMES</a>
+                        </li>
+                        <li>
+                            <a href="">COLLECTIBLES</a>
+                        </li>
+                        <li>
+                            <a href="">VIDEOS</a>
+                        </li>
+                        <li>
+                            <a href="">FANS</a>
+                        </li>
+                        <li>
+                            <a href="">NEWS</a>
+                        </li>
+                        <li>
+                            <a href="">SHOP</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
