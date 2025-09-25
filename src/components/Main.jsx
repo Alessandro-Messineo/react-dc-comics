c91d1f26941303ed28417cf653664b1e14ce1e93
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-25 16:51:48 +0200

    spostato in una nuova componente i link dei servizi

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,6 @@
+// importo Services
+import Services from "./sub-components/Services"
+
 // funzione per ritornare codice html
 const Main = () => {
     return (
@@ -9,32 +12,8 @@ const Main = () => {
                 <div>
                     <h1>--&#62; Content goes here &#60;--</h1></div>
             </div>
-            <div class="service-links">
-                <nav class="list-services">
-                    <ul >
-                        <li>
-                            <img src="../../src/assets/img/buy-comics-digital-comics.png" alt="Digital Comics" />
-                            <a href="">DIGITAL COMICS</a>
-                        </li>
-                        <li>
-                            <img src="../../src/assets/img/buy-comics-merchandise.png" alt="DC Merchandise" />
-                            <a href="" >DC MERCHANDISE</a>
-                        </li>
-                        <li>
-                            <img src="../../src/assets/img/buy-comics-subscriptions.png" alt="Subscription" />
-                            <a href="">SUBSCRIPTION</a>
-                        </li>
-                        <li>
-                            <img src="../../src/assets/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
-                            <a href="" >COMIC SHOP LOCATOR</a>
-                        </li>
-                        <li>
-                            <img src="../../assets/assets/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
-                            <a href="" >DC POWER VISA</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            {/* richiamo Services */}
+            <Services />
         </main>
     )
 }
---
+++ src/components/sub-components/Services.jsx
@@ -0,0 +1,33 @@
+// funzione per ritornare codice html
+const Services = () => {
+    return (
+        <div class="service-links">
+            <nav class="list-services">
+                <ul >
+                    <li>
+                        <img src="../../src/assets/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+                        <a href="">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <img src="../../src/assets/img/buy-comics-merchandise.png" alt="DC Merchandise" />
+                        <a href="" >DC MERCHANDISE</a>
+                    </li>
+                    <li>
+                        <img src="../../src/assets/img/buy-comics-subscriptions.png" alt="Subscription" />
+                        <a href="">SUBSCRIPTION</a>
+                    </li>
+                    <li>
+                        <img src="../../src/assets/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
+                        <a href="" >COMIC SHOP LOCATOR</a>
+                    </li>
+                    <li>
+                        <img src="../../src/assets/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
+                        <a href="" >DC POWER VISA</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Services
